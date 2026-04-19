ccd549b91ba4bfb954ac6b230275372994bc2607
Bereket A. <bereketbeki64@gmail.com>
2026-04-19 12:05:38 +0300

    Remove WeatherForecast sample and configure API services
    Removed WeatherForecastController and WeatherForecast model to eliminate sample code. Updated Program.cs to add controllers, Swagger/OpenAPI support, repository registration, and PostgreSQL DbContext configuration. Prepares the project for custom API endpoints and database integration.

--- Equipment Management/Program.cs
+++ Equipment Management/Program.cs
@@ -6,14 +6,14 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IRepository, Repository>();
-//builder.Services.AddScoped<CreateEquipmentHandler>();
+
 builder.Services.AddDbContext<EfDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
